815ea2fd27741ce1838402c317ce337adecca13a
forgotten-acolyte <melearningcpppart4@gmail.com>
2022-04-10 15:08:51 +0700

    Done AddAppointment.js and Search.js
    Done AddAppointment.js and Search.js

--- src/App.js
+++ src/App.js
@@ -2,47 +2,53 @@ import logo192 from './images/logo192.png'
 import './App.css';
 import React, {Component, useState, useEffect, useReducer} from "react";
 import {Routes, Route} from "react-router-dom";
-import { IconName,BsFillAlarmFill } from "react-icons/bs";
+import { BiCalendar, BiArchive } from "react-icons/bi";
+import Search   from "./components/Search"
+import AddAppointment from "./components/AddAppointment";
 
-import {
-  Home,
-  About,
-  Events,
-  Contacts,
-  Oops404,
-  Services,
-  CompanyHistory,
-  Location
-} from "./pages"
+// import {
+//   Home,
+//   About,
+//   Events,
+//   Contacts,
+//   Oops404,
+//   Services,
+//   CompanyHistory,
+//   Location
+// } from "./pages"
 
 
 function App(){
   return(
-    <div>
-      <h1 > <BsFillAlarmFill /> My Appoinments </h1>
+    <div className="App container mx-auto mt-3 font-thin">
+      <h1 className="text-5xl mb-3" > 
+         <BiArchive className="inline-block text-red-400"/> My Appoinments 
+      </h1>
+      <AddAppointment />
+      <Search />
     </div>   
   )
 }
 
-function App_2(){
-  return(
-    <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
+// function App_2(){
+//   return(
+//     <div>
+//       <Routes>
+//         <Route path="/" element={<Home />} />
 
-        <Route path="/about" element={<About/>}>
-          <Route path="/services" element={<Services/>}/>
-          <Route path="/history" element={<CompanyHistory/>}/>
-          <Route path="/location" element={<Location/>} />
-        </Route>
+//         <Route path="/about" element={<About/>}>
+//           <Route path="/services" element={<Services/>}/>
+//           <Route path="/history" element={<CompanyHistory/>}/>
+//           <Route path="/location" element={<Location/>} />
+//         </Route>
 
-        <Route path="/event" element={<Events/>} />
-        <Route path="/contact" element={<Contacts/>} />
-        <Route path="*" element= {<Oops404/>} />
+//         <Route path="/event" element={<Events/>} />
+//         <Route path="/contact" element={<Contacts/>} />
+//         <Route path="*" element= {<Oops404/>} />
 
-      </Routes> 
-    </div>
-  )
-}
+//       </Routes> 
+//     </div>
+//   )
+// }
 
 export default App;
